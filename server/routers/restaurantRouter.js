921456ef4c25edf28b1da2709b2b4586a6b051e0
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2023-03-03 14:53:09 -0700

    commented out incomplete routes

--- server/routers/restaurantRouter.js
+++ server/routers/restaurantRouter.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = new express.Router();
 const { getRestaurants, getRestaurant, createRestaurant, updateRestaurant, deleteRestaurant } = require('../controllers/restaurantController');
 
-router.get('/restaurants', getRestaurants);
-router.get('/restaurants/:id', getRestaurant);
+// router.get('/restaurants', getRestaurants);
+// router.get('/restaurants/:id', getRestaurant);
 router.post('/restaurants', createRestaurant);
-router.patch('/restaurants/:id', updateRestaurant);
-router.delete('/restaurants/:id', deleteRestaurant);
+// router.patch('/restaurants/:id', updateRestaurant);
+// router.delete('/restaurants/:id', deleteRestaurant);
 
 module.exports = router; 
